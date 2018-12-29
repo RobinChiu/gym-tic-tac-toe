a764644712b98190663ea55a28eacada26df9e29
Robin Chiu <robin_chiu@asus.com>
2018-12-29 23:30:34 +0800

    Add a agent to be a better enemy.

--- gym_tic_tac_toe/envs/tic_tac_toe_env.py
+++ gym_tic_tac_toe/envs/tic_tac_toe_env.py
@@ -3,6 +3,74 @@ from gym import spaces
 import numpy as np
 import random
 
+class Agent() :
+    def __init__(self, player):
+        self.player = player
+
+    def whowin(self, board):
+        result = 0
+        data = [[0, 1, 2], [3, 4, 5], [6, 7, 8], 
+                [0, 3, 6], [1, 4, 7], [2, 5, 8],
+                [0, 4, 8], [2, 4, 6]]
+        for i, j, k in data:
+            if board[i]!=0 and board[i]==board[j] and board[j]==board[k]:
+                if board[i] == self.player:
+                    result = 1
+                else:
+                    result = -1
+        return result
+
+    def isfinish(self, board):
+        for i in board:
+            if i == 0:
+                return False;
+        return True
+
+
+    def minimax(self, board, flag):
+        if self.whowin(board) == 1:
+            return 1
+        elif self.whowin(board) == -1:
+            return -1
+        elif self.isfinish(board):
+            return 0
+        
+        result = []
+        for i in range(9):
+            if board[i] == 0:
+                board[i] = flag
+                result.append(self.minimax(board, -flag))
+                board[i] = 0
+        if flag==self.player:
+            return max(result)
+        else:
+            return min(result) 
+
+    def get_random(self):
+        result = [i for i in range(9)]
+        for i in range(9):
+            j = random.randint(0, 8)
+            temp = result[i]
+            result[i] = result[j]
+            result[j] = temp
+        return result
+
+    def next_pos(self, board):
+        max_value = -10
+        pos = -1
+        post_list = self.get_random()
+        for i in post_list:
+            if board[i]==0:
+                board[i]=self.player
+                value = self.minimax(board, -self.player)
+                board[i]=0
+                if value > max_value:
+                    max_value = value
+                    pos = i
+        return pos
+
+
+
 class TicTacToeEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
@@ -10,6 +78,8 @@ class TicTacToeEnv(gym.Env):
         self.action_space = spaces.Discrete(9)
         high = np.array([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0])
         self.observation_space = spaces.Box(-high, high) # flattened
+        self.agent = Agent(-1)
+        self.use_agent = False
 
     def _seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
@@ -24,6 +94,8 @@ class TicTacToeEnv(gym.Env):
             empty_pos = [i for i, j in enumerate(state["board"]) if j == 0]
             # print(empty_pos)
             pos = random.choice(empty_pos)
+            if self.use_agent:
+                pos = self.agent.next_pos(state["board"])
             state, reward, done, others =  self._step1([-1, pos])
 
         return np.array(state["board"]), reward, done, others
@@ -73,6 +145,12 @@ class TicTacToeEnv(gym.Env):
         self.state = {}
         self.state['board'] = [0, 0, 0, 0, 0, 0, 0, 0, 0]
         self.state['on_move'] = 1
+        if random.randint(0, 1) == 0:
+            self.use_agent = False
+        else:
+            self.use_agent = True
+
+
         return np.array(self.state["board"])
     def _render(self, mode='human', close=False):
         if close:
